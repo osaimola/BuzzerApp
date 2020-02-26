77938a386a638cdb522e37abb8136a11fb1baf23
Osa <osaimola@yahoo.com>
2020-02-26 12:53:49 -0700

    fix bug where read_file returned empty strings

--- Lambda.py
+++ Lambda.py
@@ -11,59 +11,59 @@ import boto3
 def lambda_handler(event, context):
     print("received: " + str(event))
 
-    # tempfile will hold passes for us to work on, file will be used to save to bucket.
+    # specify file paths and names for s3 bucket
+    # temp_filename will temporarily hold downloads for processing
+    # lampda_path will hold data for upload
     bucket = "buzz-bucket"
     file_name = "hello.txt"
     s3_path = "/" + file_name
     temp_filename = "/tmp/boxx.txt"
     lambda_path = "/tmp/" + file_name
 
-    # create_file takes content string and saves it to a file specified in path
+    # create_file takes a string content and saves it to a file specified in path
     def create_file(content, path):
         with open(path, 'w+') as file:
             file.write(content)
             file.close()
 
-    # TODO: fix bug where read_file function successfully downloads
-    # password file from s3 but returns an empty string when used
-    def read_file(target_content, path):
+    # read_file takes a file path and returns the string content of the file
+    def read_file(path):
+        temp_string = ""
         with open(path, 'r') as file:
             for line in file:
-                target_content += line
+                temp_string += line + " "
             file.close()
+        return temp_string
 
-    # if this request came from a call and has DTMF tones in Digits
+    # if this request cames from a call and has DTMF tones in Digits
     if "Digits" in event:
         access_code = event['Digits']
         pass_string = ""
 
-        # if master code unlock
+        # if master code, unlock
         if access_code == os.environ['MASTER_CODE']:
             return 'master'
 
-        # if one time delete from list and unlock, if null call for access
+        # if 0 is passed request for access
         elif access_code == '0':
             return 'guest'
 
+        # if one time passcode; verify, delete code from passcode list and unlock
         else:
             # read key file from s3
             s3 = boto3.client('s3')
             s3.download_file(bucket, s3_path, temp_filename)
-            #read_file(pass_string, temp_filename)
-            with open(temp_filename, 'r') as file:
-                for line in file:
-                    pass_string += line
-                file.close()
+            pass_string += read_file(temp_filename)
 
             # convert pass_string to list called 'otp'
             otp = pass_string.split(" ")
             print(otp)
 
             if access_code in otp:
-                # delete the one time pass from list, write to os.environ
+                # delete the one time pass from list
                 otp.remove(access_code)
                 pass_string = " ".join(otp)
-                # update and write file to s3\
+                # update and write file to s3
                 create_file(pass_string, lambda_path)
                 response = s3.upload_file(lambda_path, bucket, s3_path)
                 return 'friend'
@@ -71,7 +71,8 @@ def lambda_handler(event, context):
             else:
                 return 'stranger'
 
-    # else if other request from twilio via authorized number, create a validator & parse data
+    # if no Digits and request from twilio via authorized number,
+    # this is a CREATE< READ< DELETE request. create a validator & parse data
     elif u'twilioSignature' in event and u'Body' in event and event['From'] == os.environ['MY_NUMBER']:
 
         form_parameters = {
@@ -99,11 +100,7 @@ def lambda_handler(event, context):
                 s3 = boto3.client('s3')
                 s3.download_file(bucket, s3_path, temp_filename)
 
-                #read_file(pass_string, temp_filename)
-                with open(temp_filename, 'r') as file:
-                    for line in file:
-                        pass_string += line
-                    file.close()
+                pass_string += read_file(temp_filename)
 
                 create_file(pass_string, lambda_path)
                 response = s3.upload_file(lambda_path, bucket, s3_path)
@@ -118,11 +115,7 @@ def lambda_handler(event, context):
                 s3 = boto3.client('s3')
                 s3.download_file(bucket, s3_path, temp_filename)
 
-                #read_file(pass_string, temp_filename)
-                with open(temp_filename, 'r') as file:
-                    for line in file:
-                        pass_string += line
-                    file.close()
+                pass_string += read_file(temp_filename)
 
                 print("read success!")
                 print(pass_string)
@@ -136,11 +129,7 @@ def lambda_handler(event, context):
                 s3 = boto3.client('s3')
                 s3.download_file(bucket, s3_path, temp_filename)
 
-                #read_file(pass_string, temp_filename)
-                with open(temp_filename, 'r') as file:
-                    for line in file:
-                        pass_string += line
-                    file.close()
+                pass_string += read_file(temp_filename)
 
                 passes = pass_string.split(" ")
                 print(passes)
